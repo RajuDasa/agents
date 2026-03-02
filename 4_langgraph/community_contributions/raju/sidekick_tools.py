df46eb7ddbdf944583576b7b0e380287d34e5451
Raju Dasa <36834333+RajuDasa@users.noreply.github.com>
2026-03-02 18:59:50 +0530

    Update files with exercise task
    Add planner node and use sqlite as checkpointer memory.

---
+++ 4_langgraph/community_contributions/raju/sidekick_tools.py
@@ -0,0 +1,57 @@
+from playwright.async_api import async_playwright
+from langchain_community.agent_toolkits import PlayWrightBrowserToolkit
+from dotenv import load_dotenv
+#from langchain.agents import Tool
+from langchain_community.agent_toolkits import FileManagementToolkit
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.tools import GoogleSerperRun
+from langchain_experimental.tools import PythonREPLTool
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from email_sender import send_email
+
+
+load_dotenv(override=True)
+
+serper = GoogleSerperAPIWrapper()
+wikipedia = WikipediaAPIWrapper()
+
+async def playwright_tools():
+    playwright = await async_playwright().start()
+    browser = await playwright.chromium.launch(headless=False)
+    toolkit = PlayWrightBrowserToolkit.from_browser(async_browser=browser)
+    return toolkit.get_tools(), browser, playwright
+
+# def push(text: str):
+#     """Send a push notification to the user"""
+#     requests.post(pushover_url, data = {"token": pushover_token, "user": pushover_user, "message": text})
+#     return "success"
+
+def get_file_tools():
+    toolkit = FileManagementToolkit(root_dir="sandbox")
+    return toolkit.get_tools()
+
+
+def other_tools():
+    #email_tool = Tool(name="send_email", func=send_email, description="Use this tool when you want to send an email")
+    file_tools = get_file_tools()
+
+    # tool_search =Tool(
+    #     name="search",
+    #     func=serper.run,
+    #     description="Use this tool when you want to get the results of an online web search"
+    # )
+
+    tool_search = GoogleSerperRun(api_wrapper=serper)
+    wiki_tool = WikipediaQueryRun(api_wrapper=wikipedia)
+    python_repl = PythonREPLTool()
+    
+    return file_tools + [send_email, tool_search, python_repl,  wiki_tool]
+
+def tool_purpose(all_tools=[]):
+    tools = all_tools if all_tools else other_tools()
+    msg = "Below are the tools with their name and description:\n"
+    for tool in tools:
+        msg += f"Name:{tool.name} - Description:{tool.description}\n"
+    
+    return msg
